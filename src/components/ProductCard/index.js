14e984a13de2c3f7fcdfe98bd4c71968b816637e
ubirajarapelli <ubirajaracpelli@gmail.com>
2021-07-18 17:36:10 -0300

    Add ProductCard Component and Styles

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { ProductCardElement, ProductCardImage, ProductCardText } from './style'
+import { AddToCart } from '../AddToCart'
+import { ProductCardElement, ProductCardImage, ProductCardTPrice, ProductCardText, ProductCardTitle } from './style'
 
 export const ProductCard = ({ productImage, altText, productText, productPrice }) => {
+
+  const titleRegx = /^[^-]*[^ -]/
+  const splitText = productText.match(titleRegx)
+  const productTitle = splitText[0];
+  const productDescription = splitText.input;
+
+
+  const formatedPrice = (value) => {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
+  }
+
   return (
     <ProductCardElement>
+      <ProductCardTitle>{productTitle}</ProductCardTitle>
       <ProductCardImage src={productImage} alt={altText} />
-      <ProductCardText>{productText}</ProductCardText>
-      <ProductCardText>{productPrice}</ProductCardText>
+      <ProductCardText>{productDescription}</ProductCardText>
+      <ProductCardTPrice>{formatedPrice(productPrice)}</ProductCardTPrice>
+      <AddToCart />
     </ProductCardElement>
   )
 }
--- src/components/ProductCard/style.js
+++ src/components/ProductCard/style.js
@@ -1,12 +1,46 @@
 import styled from 'styled-components'
 
 export const ProductCardElement = styled.div`
-  width: 256px;
-  text-align: center;
+  /* width: 256px; */
+  display: flex;
+  flex-direction: column;
+  width: 260px;
+  margin-top: 2em;
+  margin-bottom: 2em;
+  padding: 1em;
+  border: 1px solid var(--brand-primary-light);
+  border-radius: 16px;
+  transition: all .3s ease-in;
+  &:hover {
+    box-shadow: 0 0 15px rgba(52, 36, 12, .3);
+    background-color: var(--brand-primary-light);
+  }
 `
-
 export const ProductCardImage = styled.img`
-  max-height: 160px;
+  max-width: 180px;
+  height: 180px;
+  margin: 1em auto;
 `
 
-export const ProductCardText = styled.p``
+export const ProductCardTPrice = styled.p`
+  font-family: 'Oswald';
+  font-weight: 600;
+  font-size: 18px;
+  color: var(--brand-primary-dark);
+`
+
+export const ProductCardText = styled.p`
+  height: 2em;
+  margin-bottom: 1em;
+  font-family: 'Roboto Condensed';
+  color: var(--text-dark);
+`
+
+export const ProductCardTitle = styled.h2`
+  height: 2.5em;
+  margin-bottom: 8px;
+  line-height: 1.2;
+  font-family: 'Oswald';
+  font-weight: 600;
+  font-size: 22px;
+`
