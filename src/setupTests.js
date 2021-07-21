82a8a157394feb4ce7105dd553a00b773bcbb89d
ubirajarapelli <ubirajaracpelli@gmail.com>
2021-07-20 23:28:10 -0300

    Add console.mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+const consoleMocks = {
+  log: jest.fn(),
+  error: jest.fn(),
+  table: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+};
+
+(global).console = consoleMocks;
